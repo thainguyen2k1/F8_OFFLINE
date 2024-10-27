077386a386d9fc01fb7842d84a1e1bda0881843d
Thai Nguyen <thaicute2808@gmail.com>
2024-10-28 00:10:12 +0700

    getlink

---
+++ tu_luyen/getlink/index.js
@@ -0,0 +1,27 @@
+var number = document.querySelector(".number");
+var btn = document.querySelector("button");
+window.addEventListener("load", () => {
+  setInterval(() => {
+    if (number.innerText > 0) {
+      number.innerText--;
+    } else {
+      btn.disabled = false;
+      btn.style.cursor = "pointer";
+
+      btn.style.background = "transparent";
+      btn.addEventListener("click", function() {
+        window.open("https://www.facebook.com/");
+      });
+    }
+  }, 1000);
+});
+document.addEventListener("visibilitychange", function() {
+  if (document.visibilityState === "hidden") {
+    var a = number.innerText;
+    setInterval(() => {
+      if (document.visibilityState === "hidden" && number.innerText <= a) {
+        number.innerText++;
+      }
+    }, 1000);
+  }
+});
